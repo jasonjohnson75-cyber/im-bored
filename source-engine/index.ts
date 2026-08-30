6cd0f5121e7799d7087e0139b407cf156e88517e
jasonjohnson75-cyber <jasonjohnson75@gmail.com>
2026-08-30 19:12:34 -0400

    Export Wave 3 source adapters

--- source-engine/index.ts
+++ source-engine/index.ts
@@ -6,4 +6,5 @@ export * from './adapters/ticketmaster';
 export * from './adapters/structuredHtml';
 export * from './adapters/wave1HtmlSources';
 export * from './adapters/wave2Sources';
+export * from './adapters/wave3Sources';
 export * from './extractors/jsonLdEvents';
