85c69776194359d7038f5bdfc0df84b81244f620
jasonjohnson75-cyber <jasonjohnson75@gmail.com>
2026-09-11 22:45:41 -0400

    Improve src/main.tsx for audit repairs

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './styles.css';
 import './patch.css';
+import './audit-fixes.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
